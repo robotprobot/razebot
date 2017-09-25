af7b455b1761eea50c8f1230a81cd8501de5cdfc
robotprobot <swheeler2013@outlook.com>
2017-09-25 21:19:53 +0100

    test

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ client.on("guildMemberAdd", (member) => { // Preparing the STATSTRACK file for a
   if (!playerData.exists()) { // If the file does not already exist (i.e a brand new user), generate file
     console.log("New client detected. Generating stats file.") // Alert in console that this has happened
     var stream = fs.createWriteStream(playerData); // Create the file and prepare it
-    stream.once('open', function(fd)) { // Open the file to write to it
+    stream.once('open', function(fd) { // Open the file to write to it
       stream.write('{\n'); // Write the basic template
       stream.write('  "playername": ' + member + '\n'); // Include the UserID in file for reading later
       stream.write('  "points": 0\n');
@@ -45,7 +45,7 @@ client.on("guildMemberAdd", (member) => { // Preparing the STATSTRACK file for a
       stream.end(); // Close the file and save
     };
   };
-});
+};
 
 fs.readdir("./commands/", (err, files) => { // Read the commands folder and prepare commands for use
   if (err) return console.error(err); // If reading fails, write to console and abort
