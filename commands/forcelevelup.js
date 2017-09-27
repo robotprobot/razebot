0b0f2fe42594c9d2923e67843f333e7b0b7df4e1
robotprobot <swheeler2013@outlook.com>
2017-09-27 18:19:08 +0100

    disable forcelevelup

--- commands/forcelevelup.js
+++ commands/forcelevelup.js
@@ -5,24 +5,24 @@ const config = require("../config.json"); // Require access to the config.json
 const fs = require("fs");
 
 exports.run = (client, message, args) => {
-  var userID = message.author.id; // Get userID
-  var file = require(`../stats/${userID}.json`); // Find the file that matches the userID
-  if (userID == config.ownerID) {
+  //var userID = message.author.id; // Get userID
+  //var file = require(`../stats/${userID}.json`); // Find the file that matches the userID
+  //if (userID == config.ownerID) {
     // level up
-    file.points = +25; // Add 25 points
-    file.level = +1; // Increase level by 1 (which is 25 points)
-    fs.writeFile(file, function (err) {
-      if (err) {
-        console.log("An error occured when force levelling up. Likely could not change file.");
-        message.channel.send("An error has occured. See console for details.");
-        return;
-      }
-      console.log("Force level up was used and completed successfully.")
-      message.channel.send("Force level up complete. New level is: " + userFile.level);
-    });
-  } else {
-    // deny level up
-    message.channel.send("Insufficient permissions");
-    console.log("Someone attempted to force level up with insufficient permissions. Denied.");
-  };
+  //  file.points = +25; // Add 25 points
+  //  file.level = +1; // Increase level by 1 (which is 25 points)
+  //  fs.writeFile(file, function (err) {
+  //    if (err) {
+  //      console.log("An error occured when force levelling up. Likely could not change file.");
+  //      message.channel.send("An error has occured. See console for details.");
+  //      return;
+  //    }
+  //    console.log("Force level up was used and completed successfully.")
+  //    message.channel.send("Force level up complete. New level is: " + userFile.level);
+  //  });
+  //} else {
+  //  // deny level up
+  //  message.channel.send("Insufficient permissions");
+  //  console.log("Someone attempted to force level up with insufficient permissions. Denied.");
+  //};
 }; // Go back to main.js
