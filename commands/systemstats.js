763efbdef77ef21ea13f073200e1109fb8872194
robotprobot <swheeler2013@outlook.com>
2017-09-30 14:12:29 +0100

    test

--- commands/systemstats.js
+++ commands/systemstats.js
@@ -1,10 +1,11 @@
 // This will show statistics about the bot
 
 const Discord = require("discord.js"); // Require Discord.js for app to run
+const main = require("../main.js"); // Allowing access to main.js for variable access
 const config = require("../config.json"); // Require access to the config.json
 
 exports.run = (client, message, args) => {
-  message.channel.send("Amount of unrecognised commands used: " + unrecognisedCommands);
+  message.channel.send("Amount of unrecognised commands used: " + main.unrecognisedCommands);
 
 
 }; // Go back to main.js
