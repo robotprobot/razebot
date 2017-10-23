82b358e3b5dc3790032136c879114b36b3f6f3f3
robotprobot <swheeler2013@outlook.com>
2017-10-23 22:54:57 +0100

    Removed dead code, cleaned up, rewrote some modules, extended on the logging system.

--- commands/forcelevelup.js
+++ commands/forcelevelup.js
@@ -6,31 +6,51 @@ const fs = require("fs"); // Allow filesystem read and write
 
 exports.run = (client, message, args) => {
   var userID = message.author.id; // Get userID
-  var file = require(`../stats/${userID}.json`); // Find the file that matches the userID
-  if (userID == config.ownerID) {
-    // level up
-    file.points = +25; // Add 25 points
-    file.level = +1; // Increase level by 1 (which is 25 points)
+  const userFile = require(`../stats/${userID}.json`); // Find the file that matches the userID
+
+  if (userID !== config.ownerID && (!config.loggingEnabled == "TRUE" || !config.loggingPerms == "TRUE")) { // If user is not owner AND logging is disabled
+    message.channel.send("Insufficient permissions");
+    return;
+  }
+
+  else if (userID !== config.ownerID && config.loggingEnabled == "TRUE" && config.loggingPerms == "TRUE") { // If user is not owner AND logging is enabled
+    message.channel.send("Insufficient permissions -- This incident has been logged.");
+    fs.appendFile('./log.txt', '\n' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) - * - [ PERMS ] - " + '~INSUFFICIENT PERMISSIONS~ Someone attempted to force level up without correct perms! User: ' + message.author.username + '. (ID: ' + message.author.id + ')');
+    return;
+  }
+
+  else if (userID == config.ownerID && (!config.loggingEnabled == "TRUE" || !config.loggingPerms == "TRUE")) { // If user is owner and logging is disabled
+    userFile.points = +25; // Add 25 points
+    userFile.level = +1; // Increase level by 1 (which is 25 points)
     fs.writeFile(file, function (err) {
       if (err) {
         console.log("An error occured when force levelling up. Likely could not update file.");
         message.channel.send("An error has occured. See console for details.");
-        fs.appendFile('./log.txt', '\n' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) - * - [FRCELVL] - " + 'Force level failed on user: ' + message.author.username + '. (ID: ' + message.author.id + ')');
-        return;
-      } else {
+      }
+      else {
+        console.log("Force level up was used and completed successfully.");
+        message.channel.send("Force level up complete. New level is: " + userFile.level);
+      }
+    });
+    return;
+  }
+
+  else if (userID == config.ownerID && config.loggingEnabled == "TRUE" && config.loggingPerms == "TRUE") { // if user is owner and logging is enabled
+    userFile.points = +25; // Add 25 points
+    userFile.level = +1; // Increase level by 1 (which is 25 points)
+    fs.writeFile(file, function (err) {
+      if (err) {
+        console.log("An error occured when force levelling up. Likely could not update file.");
+        message.channel.send("An error has occured. See console for details.");
+        fs.appendFile('./log.txt', '\n' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) - * - [FRCELVL] - " + 'Force level was failed on user: ' + message.author.username + '. (ID: ' + message.author.id + ')');
+      }
+      else {
         console.log("Force level up was used and completed successfully.");
         message.channel.send("Force level up complete. New level is: " + userFile.level);
         fs.appendFile('./log.txt', '\n' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) - * - [FRCELVL] - " + 'Force level was completed on user: ' + message.author.username + '. (ID: ' + message.author.id + ')');
       }
     });
-  } else {
-    // deny level up
-    console.log('Someone attempted to force level up without correct perms! User: ' + message.author.username + '. (ID: ' + message.author.id + ')');
-    if (config.loggingPerms == "TRUE") {
-      message.channel.send("Insufficient permissions -- This incident has been logged.");
-      fs.appendFile('./log.txt', '\n' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) - * - [ PERMS ] - " + '~INSUFFICIENT PERMISSIONS~ Someone attempted to force level up without correct perms! User: ' + message.author.username + '. (ID: ' + message.author.id + ')');
-    } else {
-      message.channel.send("Insufficient permissions");
-    }
+    return;
   };
+
 }; // Go back to main.js
--- main.js
+++ main.js
@@ -4,19 +4,22 @@
    MADE BY STEVEN WHEELER (robotprobot#8211)
 
    main.js is the main file for the bot
-   included should be a commands folder, and a config.json.
+   included should be a commands folder, a soundfiles folder, and a config.json.
 
    Bot is partially modifiable and can be changed via config.json.
    (Do not change the token in config.json. This will break the connection to Discord!)
 */
 
-// <<<--- Code starts past this line! --->>>
+// <<<--- Variables start past this line! --->>>
 
 const Discord = require("discord.js"); // Require Discord.js for app to run
 const client = new Discord.Client(); // Prepare a client for the bot
 const config = require("./config.json"); // Require the config file for the bot
 const fs = require("fs"); // Prepare file reading
-const version = "1.0.0";
+const mainVersion = "1.0.0";
+const statstrackVersion = "1.1.0";
+
+// <<<--- Code starts past this line! --->>>
 
 client.login(config.loginToken); // Connect to the Discord service and provide bots identity to server
 
@@ -35,14 +38,21 @@ client.on("ready", () => { // Once bot has connected and initialised, do this pa
   console.log(""); // Spacing
   console.log(config.botName + " online and ready!");
   console.log(""); // Spacing
-  console.log('"RazeBot Discord Bot Framework System" - ' + version);
-  console.log("Developed by robotprobot (Steven Wheeler)");
+  console.log('"RAZEBOT Discord Bot Framework" - V' + mainVersion);
+  console.log('"STATSTRACK Statistics Tracking System" - V' + statstrackVersion);
+  console.log(""); // Spacing
+  console.log("RAZEBOT and STATSTRACK is developed by robotprobot (Steven Wheeler)");
   console.log("DISCORD: robotprobot#8211");
   console.log("TWITTER: @robot_probot");
+  console.log("GITHUB: https://github.com/robotprobot/razebot");
   console.log(""); // Spacing
   console.log("Listening for commands with the " + config.prefix + " prefix!");
   console.log(""); // Spacing
-  client.user.setGame('on ' + version + '. Ready!');
+  client.user.setGame('on ' + mainVersion + '. Ready!');
+
+  if (config.loggingEnabled !== "TRUE") { // Alert about logging disabled
+    console.log("Logging is disabled in config.json! No logging will occur.");
+  }
 
   /* CHECK IF DIRECTORIES EXIST ON BOOT */
   // SOUNDFILES DIRECTORY
@@ -61,13 +71,13 @@ client.on("ready", () => { // Once bot has connected and initialised, do this pa
     fs.mkdirSync('./stats/');
   }
   // LOGS FILE
-  if (!fs.existsSync('./log.txt')) {
+  if (!fs.existsSync('./log.txt') && config.loggingEnabled == "TRUE") {
     console.log("Log file was not found, generating...");
     var stream = fs.createWriteStream('./log.txt');
     stream.once('open', function(fd) { // Open the file to write to it
-      stream.write('Log generated on ' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) -- You can modify what actions are logged in config.json");
+      stream.write('Log generated on ' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) -- You can modify what actions are logged, or turn off logging completely in config.json");
       stream.end(); // Close the file and save
-    });
+ });
 
   fs.readdir("./commands/", (err, files) => { // Read the commands folder and prepare commands for use
   if (err) return console.error(err); // If reading fails, write to console and abort
@@ -80,7 +90,7 @@ client.on("ready", () => { // Once bot has connected and initialised, do this pa
 };
 
 setTimeout(function() { // Bot boot logger
-  if (config.loggingStartup == "TRUE") {
+  if (config.loggingEnabled == "TRUE" && config.loggingStartup == "TRUE") {
   fs.appendFile('./log.txt', '\n' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) -   - [STARTUP] - " + "Bot booted successfully.");
     }
   }, 500);
@@ -88,10 +98,16 @@ setTimeout(function() { // Bot boot logger
 
 client.on("guildCreate", guild => { // Notes in console when bot has joined a server
   console.log(`Joined server joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  if (config.loggingEnabled == "TRUE" && config.loggingConnect == "TRUE") {
+    fs.appendFile('./log.txt', '\n' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) - * - [CONNECT] - " + 'Joined server joined: ' + guild.name + ' (id: '+ guild.id + '). This guild has ' + guild.memberCount + ' members!');
+  };
 });
 
 client.on("guildDelete", guild => { // Notes in console when bot has left or been removed from a server
   console.log(`Disconnected/removed from: ${guild.name} (id: ${guild.id})`);
+  if (config.loggingEnabled == "TRUE" && config.loggingConnect == "TRUE") {
+    fs.appendFile('./log.txt', '\n' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) - * - [CONNECT] - " + 'Disconnected/removed from: ' + guild.name + ' (id: '+ guild.id + ').');
+  };
 });
 
 client.on("guildMemberAdd", member => { // Preparing the STATSTRACK file for a joining member if new
@@ -114,16 +130,6 @@ client.on("guildMemberAdd", member => { // Preparing the STATSTRACK file for a j
   };
 });
 
-client.on("voiceJoin", function(user, voiceChannel) { // When someone joins a voice room
-  if (!voiceChannel == config.tournamentJoinRoomID) return; // If voice room is not the designated room, reject
-  const mainChannel = client.channels.get(config.tournamentJoinRoomID); // Prepare the designated channels ID
-  mainChannel.join() // Join the designated channel
-   .then(connection => { // Connection is an instance of VoiceConnection - Begin a instance
-    const dispatcher = connection.playFile('./soundfiles/aplayerjoined.mp3'); // Play the mp3
-   })
-   .catch(console.log); // Catch errors and write to console
-});
-
 client.on("message", message => { // Read messages and run the correct command if possible
   if (!message.guild) return; // If message is not in server (like a dm), reject
   if (message.author.bot) return; // If the message the bot wants to respond to is from itself, reject to prevent loops
@@ -136,17 +142,27 @@ client.on("message", message => { // Read messages and run the correct command i
     let commandFile = require(`./commands/${command}.js`); // Search for a corrosponding command
     commandFile.run(client, message, args); // If command exists, run it
     console.log('Command "' + command + '" was found successfully and used by ' + message.author.username + '. (ID: ' + message.author.id + ')');
-    if (config.loggingCommand == "TRUE") {
+    if (config.loggingEnabled == "TRUE" && config.loggingCommand == "TRUE") {
       fs.appendFile('./log.txt', '\n' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) -   - [COMMAND] - " + 'Command "' + command + '" was found successfully and used by ' + message.author.username + '. (ID: ' + message.author.id + ')');
-    }
+    };
     return;
   } catch (err) { // Else tell user that command was not found
     //console.error(err);
     console.log('Command "' + command + '" was not found, requested by ' + message.author.username + '. (ID: ' + message.author.id + ')');
     message.channel.send("Command not recognised");
-    if (config.loggingCommand == "TRUE") {
+    if (config.loggingEnabled == "TRUE" && config.loggingCommand == "TRUE") {
       fs.appendFile('./log.txt', '\n' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) -   - [COMMAND] - " + 'Command "' + command + '" was not found, requested by ' + message.author.username + '. (ID: ' + message.author.id + ')');
-    }
+    };
     return;
   }
+});
+
+client.on("voiceJoin", function(user, voiceChannel) { // When someone joins a voice room
+  if (!voiceChannel == config.tournamentJoinRoomID) return; // If voice room is not the designated room, reject
+  const mainChannel = client.channels.get(config.tournamentJoinRoomID); // Prepare the designated channels ID
+  mainChannel.join() // Join the designated channel
+   .then(connection => { // Connection is an instance of VoiceConnection - Begin a instance
+    const dispatcher = connection.playFile('./soundfiles/aplayerjoined.mp3'); // Play the mp3
+   })
+   .catch(console.log); // Catch errors and write to console
 });
