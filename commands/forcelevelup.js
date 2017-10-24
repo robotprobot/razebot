c61ceed222a36a1362e9dc51d08d79c4734c9ac1
robotprobot <swheeler2013@outlook.com>
2017-10-24 15:07:20 +0100

    Forcelevelup system is now completely functional with the sql database, new help command, and added sql closers.

--- commands/forcelevelup.js
+++ commands/forcelevelup.js
@@ -21,26 +21,31 @@ exports.run = (client, message, args) => {
 
   else if (userID == config.ownerID && (!config.loggingEnabled == "TRUE" || !config.loggingPerms == "TRUE")) { // If user is owner and logging is disabled
     sql.get(`SELECT * FROM stats WHERE userId ="${message.author.id}"`).then(row => {
+      sql.open("./stats.sqlite");
       sql.run(`UPDATE stats SET points = ${row.points + 25} WHERE userId = ${message.author.id}`);
       sql.run(`UPDATE stats SET level = ${row.level + 1} WHERE userId = ${message.author.id}`);
      }).catch(() => {
      });
-    //userFile.points = userFile.points + 25; // Add 25 points
-    //userFile.level = userFile.level + 1; // Increase level by 1 (which is 25 points)
+     setTimeout(function() {
       console.log("Force level up was used and completed successfully.");
-      message.channel.send("Force level up complete. New level is: " + userFile.level);
+      message.channel.send("Force level up complete.");
       fs.appendFile('./log.txt', '\n' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) - * - [FRCELVL] - " + 'Force level was completed on user: ' + message.author.username + '. (ID: ' + message.author.id + ')');
+    }, 1000);
       return;
   }
 
   else if (userID == config.ownerID && config.loggingEnabled == "TRUE" && config.loggingPerms == "TRUE") { // if user is owner and logging is enabled
-    sql.run(`UPDATE stats SET points = ${row.points + 25} WHERE userId = ${message.author.id}`);
-    sql.run(`UPDATE stats SET level = ${row.level + 1} WHERE userId = ${message.author.id}`);
-    //userFile.points = userFile.points + 25; // Add 25 points
-    //userFile.level = userFile.level + 1; // Increase level by 1 (which is 25 points)
+    sql.get(`SELECT * FROM stats WHERE userId ="${message.author.id}"`).then(row => {
+      sql.open("./stats.sqlite");
+      sql.run(`UPDATE stats SET points = ${row.points + 25} WHERE userId = ${message.author.id}`);
+      sql.run(`UPDATE stats SET level = ${row.level + 1} WHERE userId = ${message.author.id}`);
+     }).catch(() => {
+     });
+     setTimeout(function() {
       console.log("Force level up was used and completed successfully.");
-      message.channel.send("Force level up complete. New level is: " + userFile.level);
+      message.channel.send("Force level up complete.");
       fs.appendFile('./log.txt', '\n' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) - * - [FRCELVL] - " + 'Force level was completed on user: ' + message.author.username + '. (ID: ' + message.author.id + ')');
+    }, 1000);
       return;
   };
 }; // Go back to main.js
--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,31 @@ const commandsFolder = ("./commands/"); // Tells where the commands folder is to
 const fs = require("fs"); // Allow filesystem read and write
 
 exports.run = (client, message, args) => {
-  fs.readdirSync(commandsFolder).forEach(file => {
-    console.log(file);
+  var readfiles = []; // prepare an array
+  fs.readdirSync(commandsFolder).forEach(file => { // read the commands folder
+    readfiles.push(file); // put each file name in folder into the array
   });
+  finalresult1 = readfiles.toString(); // take the array and turn it into a string
+  finalresult2 = finalresult1.replace(/\/r/, '/'); // remove symbols
+  finalresult3 = finalresult2.replace(/,/g,'    '); // replace commas with a space
+  finalresult4 = finalresult3.replace(/.js/g, ''); // remove extensions
+  //console.log(finalresult4);
+  message.channel.send({embed: { // Prepare a embed
+    color: 3447003,
+    author: {
+      name: config.botName,
+      icon_url: client.user.avatarURL
+    },
+    title: "",
+    fields: [{
+      name: "Commands list",
+      value: finalresult4
+    } // Prepare message and collect response time
+  ],
+    timestamp: new Date(),
+    footer: {
+    icon_url: client.user.avatarURL,
+    text: config.botName
+  } // Finalise and send
+  }});
 }; // Go back to main.js
