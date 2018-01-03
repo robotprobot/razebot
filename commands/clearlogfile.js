9cf1e78a303732c32727fb41b4db395d490c01e0
robotprobot <swheeler2013@outlook.com>
2018-01-03 18:10:21 +0000

    Renamed 'Requestlogfile' to 'Downloadlogfile' Added a 'clearlogfile' command

---
+++ commands/clearlogfile.js
@@ -0,0 +1,51 @@
+// This needs to check that the requester has permission to do this command, then delete the current log and generate a new one.
+
+const Discord = require("discord.js"); // Require Discord.js for app to run
+const config = require("../config.json"); // Require access to the config.json
+const fs = require("fs"); // Allow filesystem read and write
+var logPath = './log.txt';
+
+exports.run = (client, message, args) => {
+  var userID = message.author.id; // Get userID
+
+    if (userID !== config.ownerID && (!config.loggingEnabled == "TRUE" || !config.loggingPerms == "TRUE")) { // If user is not owner AND logging is disabled
+      message.channel.send("Insufficient permissions");
+      return;
+    }
+
+    else if (userID !== config.ownerID && config.loggingEnabled == "TRUE" && config.loggingPerms == "TRUE") { // If user is not owner AND logging is enabled
+      message.channel.send("Insufficient permissions -- this incident has been logged.");
+      fs.appendFile('./log.txt', '\n' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) - * - [ PERMS ] - " + '~INSUFFICIENT PERMISSIONS~ Someone attempted to clean the log file without correct perms! User: ' + message.author.username + '. (ID: ' + message.author.id + ')');
+      return;
+    }
+
+    else if (userID == config.ownerID && (!config.loggingEnabled == "TRUE" || !config.loggingPerms == "TRUE")) { // If user is owner and logging is disabled
+      fs.unlinkSync(logPath);
+      console.log('Log file deleted');
+      message.channel.send('Log file was sucessfully removed, generating new...');
+      console.log('Generating new log file...');
+      message.channel.send('Log clearup completed.');
+      var stream = fs.createWriteStream('./log.txt');
+      stream.once('open', function(fd) { // Open the file to write to it
+        stream.write('Log generated on ' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) -- You can modify what actions are logged, or turn off logging completely in config.json");
+        stream.end(); // Close the file and save
+        });
+    }
+
+    else if (userID == config.ownerID && config.loggingEnabled == "TRUE" && config.loggingPerms == "TRUE") { // if user is owner and logging is enabled
+      fs.unlinkSync(logPath);
+      console.log('Log file deleted');
+      message.channel.send('Log file was sucessfully removed, generating new...');
+      console.log('Generating new log file...');
+      message.channel.send('Log clearup completed.');
+      var stream = fs.createWriteStream('./log.txt');
+      stream.once('open', function(fd) { // Open the file to write to it
+        stream.write('Log generated on ' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) -- You can modify what actions are logged, or turn off logging completely in config.json");
+        stream.end(); // Close the file and save
+      });
+      setTimeout(function() {
+        fs.appendFile('./log.txt', '\n' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) - * - [LOGEXPT] - " + 'Log was cleaned by ' + message.author.username + '. (ID: ' + message.author.id + ')');
+        }, 1000);
+      return;
+    };
+}; // Go back to main.js
--- commands/downloadlogfile.js
+++ commands/downloadlogfile.js
@@ -4,10 +4,13 @@
 const Discord = require("discord.js"); // Require Discord.js for app to run
 const config = require("../config.json"); // Require access to the config.json
 const fs = require("fs"); // Allow filesystem read and write
+var logPath = './log.txt';
 
 exports.run = (client, message, args) => {
   var userID = message.author.id; // Get userID
   var channelID = message.channel.id; // Get the ID of the channel where message was sent
+  var logStats = fs.statSync(logPath);
+  var fileSizeInBytes = logStats.size;
 
   if (channelID == config.administrationTextRoomID) {
     if (userID !== config.ownerID && (!config.loggingEnabled == "TRUE" || !config.loggingPerms == "TRUE")) { // If user is not owner AND logging is disabled
@@ -22,19 +25,29 @@ exports.run = (client, message, args) => {
     }
 
     else if (userID == config.ownerID && (!config.loggingEnabled == "TRUE" || !config.loggingPerms == "TRUE")) { // If user is owner and logging is disabled
-      message.channel.send(message.author.username + ' exported the log at ' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''), {
-        files: [
-          './log.txt'
-        ]
-      });
+      if (fileSizeInBytes <= 8388608) { // if file is below discord file size limit, then proceed.
+        message.channel.send(message.author.username + ' exported the log at ' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''), {
+          files: [
+            logPath
+          ]
+        });
+      }
+      else if (fileSizeInBytes > 8388608) {
+        message.channel.send("Log file has grew above the file size limit (8mb) on Discord. Cannot export. Recommend clearing log with the clearlogfile command.");
+      }
     }
 
     else if (userID == config.ownerID && config.loggingEnabled == "TRUE" && config.loggingPerms == "TRUE") { // if user is owner and logging is enabled
-      message.channel.send(message.author.username + ' exported the log at ' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''), {
-        files: [
-          './log.txt'
-        ]
-      });
+      if (fileSizeInBytes <= 8388608) { // if file is below discord file size limit, then proceed.
+        message.channel.send(message.author.username + ' exported the log at ' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''), {
+          files: [
+            logPath
+          ]
+        });
+      }
+      else if (fileSizeInBytes > 8388608) {
+        message.channel.send("Log file has grew above the file size limit (8mb) on Discord. Cannot export. Recommend clearing log with the clearlogfile command.");
+      }
       setTimeout(function() {
         fs.appendFile('./log.txt', '\n' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + " (UTC) - * - [LOGEXPT] - " + 'Log was exported by ' + message.author.username + '. (ID: ' + message.author.id + ')');
         }, 1000);
