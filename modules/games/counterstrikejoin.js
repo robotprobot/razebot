4245d2705ae8b88892bdcbe902a967151504975a
Steven Wheeler <swheeler2013@outlook.com>
2018-02-23 13:49:45 +0000

    Restructure and rewrite

--- modules/games/counterstrikejoin.js
+++ modules/games/counterstrikejoin.js
@@ -6,11 +6,46 @@ exports.run = (newMember) => {
 
 
   if (global.counterstrikeWaitingCount = 10) {
-    //write players to Database
-    sql.run(`INSERT INTO gameResults (gameType, player1Id, player2Id, player3Id, player4Id, player5Id, player6Id, player7Id, player8Id, player9Id, player10Id) VALUES (COUNTERSTRIKE,`global.counterstrikeWaitingList[0]`,`global.counterstrikeWaitingList[1]`,`global.counterstrikeWaitingList[2]`,`global.counterstrikeWaitingList[3]`,`global.counterstrikeWaitingList[4]`,`global.counterstrikeWaitingList[5]`,`global.counterstrikeWaitingList[6]`,`global.counterstrikeWaitingList[7]`,`global.counterstrikeWaitingList[8]`,`global.counterstrikeWaitingList[9]`)`);
+    var amountofteam1;
+    var amountofteam2;
 
     //choose teams
+    function generateTeam() {
+      var result = Math.floor(Math.random() * 2) + 1;
+      if (result == 1) && (amountofteam1 < 5) {
+        amountofteam1 = amountofteam1 + 1
+        return result;
+      }
+      else if (result == 1) && (amountofteam1 == 5) {
+        generateTeam();
+      }
+      else if (result == 2) && (amountofteam2 < 5) {
+        amountofteam2 = amountofteam2 + 1
+        return result;
+      }
+      else if (result == 2) && (amountofteam2 == 5) {
+        generateTeam();
+      }
+    }
+
+    var player1teamrandom = generateTeam(); // choose team 1 or 2
+    var player2teamrandom = generateTeam(); // choose team 1 or 2
+    var player3teamrandom = generateTeam(); // choose team 1 or 2
+    var player4teamrandom = generateTeam(); // choose team 1 or 2
+    var player5teamrandom = generateTeam(); // choose team 1 or 2
+    var player6teamrandom = generateTeam(); // choose team 1 or 2
+    var player7teamrandom = generateTeam(); // choose team 1 or 2
+    var player8teamrandom = generateTeam(); // choose team 1 or 2
+    var player9teamrandom = generateTeam(); // choose team 1 or 2
+    var player10teamrandom = generateTeam();  // choose team 1 or 2
+
+    //write to database
+    sql.run(`INSERT INTO gameResults(gameType, player1Id, player2Id, player2Team, player3Id, player3Team, player4Id, player4Team, player5Id, player5Team, player6Id, player6Team, player7Id, player7Team, player8Id, player8Team, player9Id, player9Team, player10Id, player10Team) VALUES(`global.counterstrikeWaitingList[0]`,`player1teamrandom`,`global.counterstrikeWaitingList[1]`,`player2teamrandom`,`global.counterstrikeWaitingList[2]`,`player3teamrandom`,`global.counterstrikeWaitingList[3]`,`player4teamrandom`,`global.counterstrikeWaitingList[4]`,`player5teamrandom`,`global.counterstrikeWaitingList[5]`,`player6teamrandom`,`global.counterstrikeWaitingList[6]`,`player7teamrandom`,`global.counterstrikeWaitingList[7]`,`player8teamrandom`,`global.counterstrikeWaitingList[8]`,`player9teamrandom`,`global.counterstrikeWaitingList[9]`,`player10teamrandom`)`);
+    //put the id into a variable for updating later? or we just gonna search the database?
+
     //create channels
+    
+
     //move users
     //start game
     //game
